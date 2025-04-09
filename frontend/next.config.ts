3cbcbf0931a6d00ff1a5303b6827268f83bb519d
Afifa <afifasadiq03@gmail.com>
2025-04-09 18:28:26 +0530

    next.config.ts modified to ignore eslint during builds

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ['t59vul7r5s.ufs.sh'], // <-- add this line
+    domains: ['t59vul7r5s.ufs.sh'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // ⬅️ This is the key addition
   },
 };
 
