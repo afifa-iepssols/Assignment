46cccf6f50fcb34940199f1b0b7c4da65533a44a
afifa-iepssols <afifa.iepssols@gmail.com>
2025-04-09 21:42:19 +0530

    Update page.tsx of register with server url

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -28,7 +28,7 @@ export default function RegisterPage() {
     setError({ username: '', general: '' });
 
     try {
-      const res = await fetch('http://localhost:5000/api/auth/register', {
+      const res = await fetch('https://assignment-2-tjzw.onrender.com/api/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
