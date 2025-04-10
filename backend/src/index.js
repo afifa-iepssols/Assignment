802c177f1ae290862d78d7b399308fd456b9587b
afifa-iepssols <afifa.iepssols@gmail.com>
2025-04-10 19:01:37 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-      origin: 'https://assignment-k8kht49j2-afifa-iepssols-projects.vercel.app/', 
+      origin: 'https://assignment-k8kht49j2-afifa-iepssols-projects.vercel.app', 
       credentials: true, 
     })
 );
