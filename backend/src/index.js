3b54cd7b9ba016b81fbcd0b25eea2b78f5a50d6d
afifa-iepssols <afifa.iepssols@gmail.com>
2025-04-10 18:56:30 +0530

    Update index.js with new vercel app url

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-      origin: 'https://assignment-4d05eup5u-afifa-iepssols-projects.vercel.app', 
+      origin: 'https://assignment-k8kht49j2-afifa-iepssols-projects.vercel.app/', 
       credentials: true, 
     })
 );
